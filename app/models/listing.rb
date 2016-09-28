4e07a534d1e5ef5f2530118dec092deef57fabdd
Soo Ching <soomosh@gmail.com>
2016-09-28 18:40:54 +0800

    asd

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,13 +3,11 @@ class Listing < ActiveRecord::Base
    mount_uploaders :avatars, AvatarUploader
    has_many :reservations
    searchkick match: :word_start, searchable: [:title, :location]
-   include Elasticsearch::Model
-
-
 
    #make your model searchable
     include Elasticsearch::Model
   	include Elasticsearch::Model::Callbacks
+  	index_name("#{Rails.env}")
 	# def self.count_nights
 	# 	end_date - start_date
 	# end

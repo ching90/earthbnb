ed30ac470d5ee924b39f50e544322bf60d970f27
Soo Ching <soomosh@gmail.com>
2016-09-29 11:32:53 +0800

    added action mailer in production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+ 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -52,8 +52,8 @@ Rails.application.configure do
     enable_starttls_auto:   true
 
   }
-
-  # config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  #apply above already 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: 'sooching4896@gmail.com'}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,23 @@ Rails.application.configure do
 
   Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
 
+
+  #mailer setting starts
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   587,
+    # domain:               'localhost:3000',
+    user_name:              ENV["GMAIL_USERNAME"],
+    password:               ENV["GMAIL_PASSWORD"],
+    authentication:         'plain',
+    enable_starttls_auto:   true
+
+  }
+  #apply above already 
+  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'sooching4896@gmail.com'}
+  #mailer setting ends
 end
